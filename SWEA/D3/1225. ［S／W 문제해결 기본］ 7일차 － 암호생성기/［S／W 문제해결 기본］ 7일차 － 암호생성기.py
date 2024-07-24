5774240595560916fe85e56abb48040f5cfdf8d7
Gayeon Park <99467991+UrdoingGood@users.noreply.github.com>
2024-07-24 11:26:09 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 117 ms, Memory: 44,788 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -13,3 +13,24 @@ for test_case in range(1, T + 1):
         queue.append(q) # 뺀 값이 0보다 크면 그 값을 맨 뒤에 추가
         i += 1
     print(f"#{test_case}", *queue)
+    
+'''
+# 강사님 풀이
+for _ in range(10):
+	tc = int(input())
+    input_list = list(map(int, input().split()))
+    pw_queue = collections.deque(input_list)
+    
+    reduce_cnt = 1
+    
+    while True:
+    	if reduce_cnt > 5:
+        	reduce_cnt = 1
+        
+        pop_num = pw_queue.popleft() - reduce_cnt
+        if pop_num <= 0:
+        	pw_queue.append(0)
+            break
+        pw_queue.append(pop_num)
+        reduce_cnt += 1
+'''
