f87d2c8ba2aa989a61a00216c8fefe3a22913e80
Gayeon Park <99467991+UrdoingGood@users.noreply.github.com>
2024-10-23 17:41:12 +0900

    [Gold V] Title: 숫자고르기, Time: 52 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,27 +1,31 @@
+from collections import deque
+
+def bfs(start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        next_node = graph[node] # 인덱스 번호에 해당하는 곳 방문
+
+        if next_node in visited:
+            if next_node == start: # 그 인덱스에 있는 값이 이미 방문했던 인덱스 번호와 같으면 다시 돌아왔다는 뜻 (순환)
+                for v in visited: # 여기까지 방문한 노드들을 추가
+                    result.add(v)
+            return
+
+        visited.add(next_node)
+        queue.append(next_node)
+
 N = int(input())
-
-graph = [[] for _ in range(N + 1)]
-result = []
-
-
-def dfs(v, i):
-    visited[v] = 1
-
-    for k in graph[v]:
-        if not visited[k]:
-            dfs(k, i)
-
-        elif visited[k] and k == i:
-            result.append(k)
-
+graph = [0] + [int(input()) for _ in range(N)] # 인덱스 1번부터 입력이 들어감
+result = set()
 
 for i in range(1, N + 1):
-    graph[int(input())].append(i)
-
-for i in range(1, N + 1):
-    visited = [0] * (N + 1)
-    dfs(i, i)
+    if i not in result:
+        bfs(i)
 
 print(len(result))
-for i in result:
-    print(i)
+for num in sorted(result):
+    print(num)
