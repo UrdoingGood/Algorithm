2869c40c6bb25bc99756f05f0e6a14ce89adc8f9
Gayeon Park <99467991+UrdoingGood@users.noreply.github.com>
2024-11-13 00:04:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 746 ms, Memory: 49,392 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,28 +1,33 @@
-def dfs(n, a_list, b_list):
-    global res
-    # 종료 조건 : 재료 번호 다 탐색한 경우
-    if n == N:
-        if len(a_list) == len(b_list): # a와 b 둘다 선택 완료
-            asum = bsum = 0
-            for i in range(N//2): # 선택한 재료 번호로 만들 수 있는 모든 조합의 합 구하기
-                for j in range(N//2):
-                    asum += arr[a_list[i]][a_list[j]]
-                    bsum += arr[b_list[i]][b_list[j]]
-            res = min(res, abs(asum-bsum))
-        return
-
-    dfs(n+1, a_list+[n], b_list) # a 음식에 재료 추가
-    dfs(n+1, a_list, b_list+[n]) # b 음식에 재료 추가
+import itertools
 
 T = int(input())
-for test_case in range(1, T + 1):
+for test_case in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    res = float('inf')
+    res = float('INF')
 
-    # 선택할 재료 번호 => n
-    # a 음식 만들기 => a_list
-    # b 음식 만들기 => b_list
-    dfs(0, [], [])
+    num_list = [i for i in range(N)]
+
+    all_food = itertools.combinations(num_list, N//2)
+    for a_food in all_food:
+        b_food = []
+
+        for num in num_list:
+            if num not in a_food:
+                b_food.append(num)
+
+        a_sy_list = itertools.combinations(a_food, 2)
+        a_sy_sum = 0
+        for a_sy in a_sy_list:
+            i, j = a_sy
+            a_sy_sum += (arr[i][j] + arr[j][i])
+
+        b_sy_list = itertools.combinations(b_food, 2)
+        b_sy_sum = 0
+        for b_sy in b_sy_list:
+            i, j = b_sy
+            b_sy_sum += (arr[i][j] + arr[j][i])
+
+        res = min(res, abs(a_sy_sum - b_sy_sum))
 
     print(f"#{test_case} {res}")
