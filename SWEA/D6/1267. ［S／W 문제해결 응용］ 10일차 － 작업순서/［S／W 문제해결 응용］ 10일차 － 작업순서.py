aade164e87a27a552b207924071eaf2942e15a7f
Gayeon Park <99467991+UrdoingGood@users.noreply.github.com>
2024-12-03 16:04:38 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 163 ms, Memory: 49,704 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
@@ -1,36 +1,37 @@
 from collections import defaultdict
 from collections import deque
 
-def bfs():
-    global res
-    queue = deque()
 
-    for v in range(1, V+1):
+def work():
+    queue = deque()
+    
+    for v in range(1, N):
         if pre[v] == 0:
             queue.append(v)
-
+            
     while queue:
         current = queue.popleft()
         res.append(current)
+        
         for child in graph[current]:
             pre[child] -= 1
             if pre[child] == 0:
                 queue.append(child)
 
+
 T = 10
 for test_case in range(1, T+1):
-    V, E = map(int, input().split())
+    N, M = map(int, input().split())
     edges = list(map(int, input().split()))
     graph = defaultdict(list)
-
-    pre = [0] * (V+1)
+    pre = [0] * (N+1)
     res = []
 
-    for i in range(E):
+    for i in range(M):
         a, b = edges[i * 2], edges[i * 2 + 1]
         graph[a].append(b)
         pre[b] += 1
 
-    bfs()
+    work()
 
     print(f"#{test_case}", *res)
