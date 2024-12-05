582e8a0845e26bd75f74feda941c4eb8cca51234
Gayeon Park <99467991+UrdoingGood@users.noreply.github.com>
2024-12-05 11:35:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 640 ms, Memory: 55,776 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,33 +1,21 @@
-import itertools
+def dfs(n, alist, blist):
+    global res
+    if n == N:
+        if len(alist) == len(blist):
+            asum = bsum = 0
+            for i in range(N//2):
+                for j in range(N//2):
+                    asum += arr[alist[i]][alist[j]]
+                    bsum += arr[blist[i]][blist[j]]
+            res = min(res, abs(asum-bsum))
+        return
+    dfs(n+1, alist+[n], blist)
+    dfs(n+1, alist, blist+[n])
 
 T = int(input())
-for test_case in range(1, T+1):
+for test_case in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    res = float('INF')
-
-    num_list = [i for i in range(N)]
-
-    all_food = itertools.combinations(num_list, N//2)
-    for a_food in all_food:
-        b_food = []
-
-        for num in num_list:
-            if num not in a_food:
-                b_food.append(num)
-
-        a_sy_list = itertools.combinations(a_food, 2)
-        a_sy_sum = 0
-        for a_sy in a_sy_list:
-            i, j = a_sy
-            a_sy_sum += (arr[i][j] + arr[j][i])
-
-        b_sy_list = itertools.combinations(b_food, 2)
-        b_sy_sum = 0
-        for b_sy in b_sy_list:
-            i, j = b_sy
-            b_sy_sum += (arr[i][j] + arr[j][i])
-
-        res = min(res, abs(a_sy_sum - b_sy_sum))
-
+    res = float('inf')
+    dfs(0, [], [])
     print(f"#{test_case} {res}")
